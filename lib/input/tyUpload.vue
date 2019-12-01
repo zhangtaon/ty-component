36dbc473cdddbd4d3e269c2a522c88a87ef8beaa
mzq <76350406@qq.com>
2019-12-01 17:39:09 +0800

    fix: 添加 upload组件 文件删除功能

--- lib/input/tyUpload.vue
+++ lib/input/tyUpload.vue
@@ -1,39 +1,52 @@
 <template>
-  <el-form-item :label="$attrs.label+':'">
-    <el-upload class="ty-upload" v-bind="$attrs" :on-exceed="exceed" :on-success="success">
-      <slot />
-      <slot slot="tip" name="tip"/>
-    </el-upload>
-  </el-form-item>
+    <el-form-item :label="$attrs.label + ':'">
+        <el-upload
+                class="ty-upload"
+                v-bind="$attrs"
+                :on-exceed="onExceed"
+                :on-success="onSuccess"
+                :on-remove="onRemove"
+        >
+            <slot/>
+            <slot slot="tip" name="tip"/>
+        </el-upload>
+    </el-form-item>
 </template>
 
 <script>
-export default {
-  props: {
-    // must be included in props
-   
-  },
-  methods: {
-    success(res) {
-      // console.log("success:",res);
-      if(res.code == 0){
-        this.$emit("input", res.data.length == 1 ? res.data[0].fileName : data);
-      }else{
-        this.$notify.error({
-          title: '错误',
-          message: res.msg
+  export default {
+    methods: {
+      formatFileList(fileList = []) {
+        return fileList.map(item => {
+          if (item.response && item.response.data) {
+            return item.response.data[0];
+          }
+        });
+      },
+      onRemove(file, fileList) {
+        this.asyncData(fileList);
+      },
+      onSuccess(response, file, fileList) {
+        if (response.code === 0) {
+          this.asyncData(fileList);
+        } else {
+          this.$notify.error({
+            title: "错误",
+            message: response.msg
+          });
+        }
+      },
+      asyncData(fileList) {
+        const list = this.formatFileList(fileList);
+        this.$emit("input", list);
+      },
+      onExceed(data) {
+        this.$notify.warning({
+          title: '警告',
+          message: `已超出最大上传量，最多允许上传${data.length}个文件，请删除现有文件后在进行上传!`
         });
       }
     },
-    exceed(data) {
-      this.$notify.warning({
-        title: '警告',
-        message: `已超出最大上传量，最多允许上传${data.length}个文件，请删除现有文件后在进行上传!`
-      });
-    }
-  },
-  created() {
-    // console.log("$attrs:", this.$attrs);
-  }
-};
+    created() {}
+  };
 </script>
